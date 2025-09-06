21c5bfc74ebbca28138f3819658585566c6c4109
madhu827 <srinulakkakula68@gmail.com>
2025-09-06 12:33:30 +0530

    learned datatypes in js

--- practice.js
+++ practice.js
@@ -1,1 +1,32 @@
 console.log("Hello world,madhu kumar")
+let name ="madhu";
+console.log(name);
+let num=28;
+console.log(num);
+let isLogged= false;
+let x;
+console.log(x);
+let y=null;
+console.log(y);
+console.log(isLogged);
+console.log(typeof name);
+console.log(typeof num);
+console.log(typeof isLogged);
+
+console.log(typeof x);
+console.log(typeof y);
+
+let arr = [1,2,3,4];
+console.log(arr[0]);
+console.log(typeof arr);
+
+let person ={
+    name:"madhu",
+    age:21
+
+    
+}
+console.log(person.name);
+console.log(person.age);
+console.log(typeof person);
+
